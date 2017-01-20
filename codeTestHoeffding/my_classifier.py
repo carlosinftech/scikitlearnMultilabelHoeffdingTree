42ccfdb3974fcb80a063fb6308d2a9ad8e8aaadd
Carlos Perez <carlos.inf.tech@gmail.com>
2017-01-20 10:06:57 -0500

    Update to have statistics for nominal values kind of.

--- codeTestHoeffding/my_classifier.py
+++ codeTestHoeffding/my_classifier.py
@@ -13,80 +13,41 @@ from node import Node
 class HoeffdingTreeClassifier:
 
 	def __init__(self):
-		self.j_max = 0
-		self.L = -1
 		self.root = None
-		self.xdata = None
-		self.ydata = None
-		self.h = None
-		self.H = []
+		j_max = 0
+		L = -1
 		##Infogain stuff
 		self._min_frac_weight_for_two_branches_gain = 0.01
 		self.INFO_GAIN_SPLIT = 1
 		self._selected_split_metric = self.INFO_GAIN_SPLIT
 		self._split_confidence = 0.0000001
 		self._split_metric = InfoGainSplitMetric(self._min_frac_weight_for_two_branches_gain)
+		
 
 
-	def setParent(self,node):
-		self.parent = node
 	
 	def compute_hoeffding_bound(self, max_value, confidence, weight):
 		return math.sqrt(((max_value * max_value) * math.log(1.0 / confidence)) / (2.0 * weight))
 	
 	##Just starting an empty tree and adding the x to it. 	
 	def fit(self, X, Y):
-		if not self.xdata:
-			self.xdata = X
-		##print(len(X[0]))
-		##print(len(X))
-		metric_max = self._split_metric.get_metric_range(X)
-		##print(metric_max)
-		##hoeffding_bound = self.compute_hoeffding_bound(metric_max, self._split_confidence, node.total_weight())
-		self.ws,self.L = Y.shape
-		h = PS(p=7,h=DecisionTreeClassifier())
-		h.fit(X,Y)
-		self.xpos=0
-		self.H.append(h)
-		self.s1=(self.ws,len(X[0]))
-		self.s2=(self.ws,len(Y[0]))
-		self.X_batch = zeros(self.s1)
-		self.Y_batch = zeros(self.s2)
+		N,self.L = Y.shape
+		Y_sum = sum(Y,axis=0)
+		self.j_max = argmax(Y_sum)
 		return self
 
 	def partial_fit(self, x, y=None):
+		if self.root is None:
+			self.root = Node()
 		metric_max = self._split_metric.get_metric_range(x)
-		##print('p')
-		##print(metric_max)
-		##print(x[0])
-		##print((x[0]))
-		##print(len(x))
-		##print(len(x[0]))
-		xb = False
-		xpos = self.xpos
-		self.X_batch[xpos]=x
-		self.Y_batch[xpos]=y
-		self.xpos = xpos + 1
-		
-		if xpos == self.ws-1:
-			self.xpos = 0
-			self.h = PS(p=7,h=DecisionTreeClassifier())
-			self.h.fit(self.X_batch,self.Y_batch)
-			self.H.append(self.h)
-			if len(self.H)>5:
-				self.H = self.H[-5:]
+		self.root.update_statistics(x,y)
 		return self
 
-	def predict(self, x):
-		N,D = x.shape
-		H2 = zeros((len(self.H),self.L))
-		for k in range(len(self.H)):
-			H2[k]=self.H[k].predict(x)
-		R = H2.sum(axis=0)
-		R = R * float(1/float(len(self.H)))
-		for k in range(len(R)):
-			R[k]= 1 if R[k] >= float(0.5) else 0
-		return R
+	def predict(self, X):
+		N,D = X.shape
+		Y = zeros((N,self.L))
+		Y[:,self.j_max] = 1
+		return Y
 
 class MajorityLabel:
 
