e54ecda678f3ec81d074fce3ea76627312ee3ab1
Aileen Posadas <aileen.posadas@gmail.com>
2017-02-09 19:34:22 +0100

    Update for the documentation
    I add the multiline docstrings for explaining the methods.

--- codeTestHoeffding/node.py
+++ codeTestHoeffding/node.py
@@ -1,8 +1,14 @@
+## Standard library imports
 from numpy import *
+
+##  Application specific imports
 from infogainsplitmetric import InfoGainSplitMetric
+
+"""This class creates the nodes that constucts a Hoeffding tree"""
 
 class Node():
 	def __init__(self):
+		"""Default values of a Node, constructs a node"""		
 		self.parent = None
 		self.children = None
 		self.map = None
@@ -26,18 +32,20 @@ class Node():
 		self.per_column = None
 	
 	def setParent(self,node):
+		"""Sets a node as parent"""
 		self.parent = node
 		
 	def get_node_statistics(self):
+		"""Returns the node's statistics"""
 		return self.statistics
-	
-
-						
-
-	
-	
 
 	def compute_first_second_best(self):
+		"""Look in the map for get the instance per each class and per each attribute, the instances are 
+		   saved in an array. Then is computed the method entropy (information gain in this case) om order 
+		   to obtain the attribute with highest entropy and the second highest.
+		   self.first is the attribute with highest entropy.
+		   self.second is the attribute with the second highest entropy.
+		"""
 		metric_temp = 0
 		metric_max = 0
 		self.first = 0
@@ -58,17 +66,17 @@ class Node():
 					self.column_name_first = key_class
 					self.n = len(column)
 
-	
-	##range of a variable 
-	##(e.g., for a probability the range is one, and for an information gain the range is log c, where c is the number of classes
 	def compute_hoeffding_bound(self, range, delta, n):
+		"""Computes the hoeffding bound and returns its value"""
 		return math.sqrt(((range * range) * math.log(1.0 / delta)) / (2.0 * n))
-	
-
-	
+		
 	def comparison_entropies_hf_bound(self):
-	##number of rows of r
-		##just for info gain
+		"""First computes the hoeffding bound with the numbers of clasess at the moment.
+		   then it does the substaction of the values of self.first and self.second
+		   and lastly it compares if the substraction of the entopies is greater than the hoeffding
+		   bound. if so then the node must split.
+		   Note: for the range of the variable, for information gain the range is log c, where c is the number of classes
+		"""
 		c = math.log(self.number_of_classes)
 		hoeffding_bound = self.compute_hoeffding_bound(c,self.delta, self.n)
 		entropy_Xa_Xb = self.first - self.second
@@ -79,11 +87,13 @@ class Node():
 			##print('dont split')
 		
 	def split(self,column_name):
+		"""Process of spliting a node, for each y in the map it is created a node classified as child.
+		   each child  is loaded with its belonging statistics.
+		"""
 		self.splitCondition = column_name
 		self.children = {}
 		self.activeNode = False
-		previous = 0
-		
+		previous = 0	
 		for y_val in self.map:
 				for j_val in self.map[y_val][column_name]:
 					child = Node()
@@ -102,12 +112,18 @@ class Node():
 										else:
 											child_statistics[y_val][k][k_val]+=1
 					self.children[j_val].set_statistics(self.map.copy(),child_statistics.copy())
+	
 	##temporary Need to add the real map and statistics
 	def set_statistics(self,map,statistics):
+		"""For adding a map and statistics"""
 		self.map = None
 		self.statistics = None
 	
 	def update_statistics(self,x,y):
+		"""This method fits the x and y, if the node is active then the statistics are updating according the values of x an y
+		   It is also invoqued the methods for obtaing the first and second best 
+		   if is not an active node, it goes to the child leaves and recursively is called again the method.
+		"""
 		self.number_of_classes = len(x[0])-1
 		if self.activeNode:
 			if self.statistics is None:
