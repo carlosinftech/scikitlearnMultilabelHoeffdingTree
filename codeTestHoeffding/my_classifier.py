f058e8780190b9fe38ca9fb402b54b9dc8bc036f
carlosinftech <carlos.inf.tech@gmail.com>
2017-02-09 23:55:17 -0500

    simplification it works

--- codeTestHoeffding/my_classifier.py
+++ codeTestHoeffding/my_classifier.py
@@ -13,25 +13,16 @@ class HoeffdingTreeClassifier:
 		""" Constructor of the class, contains essential parameters for the 
 		    hoeffding tree creation.
 		"""
-		self.root = None
+		self.root = Node()
 		j_max = 0
-		L = -1
-		self._min_frac_weight_for_two_branches_gain = 0.01
-		self.INFO_GAIN_SPLIT = 1
-		self._selected_split_metric = self.INFO_GAIN_SPLIT
-		self._split_confidence = 0.0000001
-		self._split_metric = InfoGainSplitMetric(self._min_frac_weight_for_two_branches_gain)
-		self.current  = None
 	
 	def fit(self, X, Y):
 		""" Just starting an empty tree and adding the x to it. """
-		N,self.L = Y.shape
-		Y_sum = sum(Y,axis=0)
-		self.j_max = argmax(Y_sum)
+		N,self.L = X.shape
+		for i in range(N-1):
+			self.partial_fit([X[i]],[Y[i]])
 		return self
-	def split_verification(self):
-	
-		return None			
+			
 	
 	def partial_fit(self, x, y=None):
 		""" constructs the root node if has not been created, fills the statistics of the root node
@@ -40,38 +31,11 @@ class HoeffdingTreeClassifier:
 		"""
 		if self.root is None:  
 			self.root = Node()
-			self.current = self.root
-			self.root.update_statistics(x,y)
 		self.root.update_statistics(x,y)	
 		return self
 
 	def predict(self, X):
 		""" for calling the prediction of the node """
-		N,D = X.shape
-		Y = zeros((N,self.L))
-		Y[:,self.j_max] = 1
-		#Y = self.node.predict(X)
-		return Y
-
-class MajorityLabel:
-
-	def __init__(self):
-		j_max = 0
-		L = -1
-
-	def fit(self, X, Y):
-		N,self.L = Y.shape
-		Y_sum = sum(Y,axis=0)
-		self.j_max = argmax(Y_sum)
-		return self
-
-	def partial_fit(self, X, Y=None):
-		return self
-
-	def predict(self, X):
-		N,D = X.shape
-		Y = zeros((N,self.L))
-		Y[:,self.j_max] = 1
-		return Y
-
+		c= self.root.predict(X)
+		return c
 
