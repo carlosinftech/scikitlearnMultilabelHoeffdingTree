a0c13d15c29ff22b70bfd42ff9c5e29417d52ef7
carlosinftech <carlos.inf.tech@gmail.com>
2017-02-10 14:51:33 -0500

    Fix error sorry :D

--- codeTestHoeffding/my_classifier.py
+++ codeTestHoeffding/my_classifier.py
@@ -1,58 +1,40 @@
 from numpy import *
-random.seed(0)
 
-print('Load data')
-import pandas as pd
+from molearn.classifiers.PS import PS
 
-df = pd.read_csv("data/ohsumed.csv",nrows=1000)#s
-L = 1
-N_train = 950
+from sklearn.tree import DecisionTreeClassifier
 
-labels = df.columns.values.tolist()[0:L]
-data = df.values
-T = len(data)
-Y = data[:,0:L]
-X = data[:,L:]
+from infogainsplitmetric import InfoGainSplitMetric
 
-print("Experimentation")
-from my_classifier import  HoeffdingTreeClassifier
-from molearn.classifiers.BR import BR
-from sklearn import linear_model
+from node import Node
 
-h = [BR(h=linear_model.SGDClassifier(n_iter=1)), HoeffdingTreeClassifier()]
 
-from molearn.core.evaluation import prequential_evaluation, get_errors
 
-E_pred,E_time = prequential_evaluation(X,Y,h,N_train)
+class HoeffdingTreeClassifier:
 
-print("Evaluation")
-from molearn.core.metrics import Exact_match, J_index
+	def __init__(self):
+		self.root = Node()
+		j_max = 0
 
-E = zeros((len(h),T-N_train))
-for m in range(len(h)):
-    E[m] = get_errors(Y[N_train:], E_pred[m],J=Exact_match)
 
-print("Plot Results")
-from matplotlib.pyplot import *
+		
+	def fit(self, X, Y):
+		N,self.L = X.shape
+		for i in range(N-1):
+			self.partial_fit([X[i]],[Y[i]])
+		return self
+				
+	
+	def partial_fit(self, x, y=None):
+		if self.root is None:  
+			self.root = Node()
+		self.root.update_statistics(x,y)	
+		return self
 
-fig, ax = subplots(2)
-w = 200
-for m in range(len(h)):
-    acc = mean(E[m,:])
-    time = mean(E_time[m,:])
-    print(h[m].__class__.__name__)
-    print("Exact Match %3.2f" % mean(acc))
-    print("Running Time  %3.2f" % mean(time))
-    print("---------------------------------------")
-    acc_run = convolve(E[m,:], ones((w,))/w,'same')#[(w-1):]
-    ax[0].plot(arange(len(acc_run)),acc_run, "-", label=h[m].__class__.__name__)
-    acc_time = convolve(E_time[m,:], ones((w,))/w,'same')
-    ax[1].plot(arange(len(acc_time)),acc_time, ":", label=h[m].__class__.__name__)
+	def predict(self, X):
+		c= self.root.predict(X)
+		return c
 
-ax[0].set_title("Accuracy (exact match)")
-ax[1].set_title("Running Time (ms)")
-ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3)
-ax[1].legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3)
-#savefig("lab2_fig.pdf")
-show()
+
+
 
