50a030216d373cc930840c41f93fc2fc4deb9a66
carlosinftech <carlos.inf.tech@gmail.com>
2017-02-10 21:30:04 -0500

    Update to have multilabel classification

--- codeTestHoeffding/my_classifier.py
+++ codeTestHoeffding/my_classifier.py
@@ -1,6 +1,5 @@
-from numpy import *
+import  numpy as np
 
-from molearn.classifiers.PS import PS
 
 from sklearn.tree import DecisionTreeClassifier
 
@@ -12,19 +11,46 @@ from node import Node
 class MultiLabelCCHoeffdingTreeClassifier:
 
 	def  __init__(self):
+		self.hoeffdingTreesList = None
+		self.predictionsList = None
 		self.h1 = HoeffdingTreeClassifier()
 	
 	def fit(self, X, Y):
 		self.h1.fit(X,Y)
+		self.hoeffdingTreesList  = []
+		self.predictionsList = []
+		for column in range(len(Y[0])):
+			yvar=np.zeros((len(Y),1))
+			for row in range(len(Y)):
+				yvar[row][0]= Y[row][column]
+			self.hoeffdingTreesList.append(HoeffdingTreeClassifier())
+			self.hoeffdingTreesList[column].fit(X,yvar)
 		return self
 	
-	def partial_fit(self,x,y=None):
-		self.h1.partial_fit(x,y)
+	def partial_fit(self,X,Y=None):
+		self.h1.partial_fit(X,Y)
+		if self.hoeffdingTreesList is None:
+			self.hoeffdingTreesList  = list(range(len(Y[0])))
+		if self.predictionsList is None:
+			self.predictionsList = list(range(len(Y[0])))
+		for column in range(len(Y[0])):
+			yvar=np.zeros((len(Y),1))
+			for row in range(len(Y)):
+				yvar[row][0]= Y[row][column]
+			if self.hoeffdingTreesList[column]:
+				self.hoeffdingTreesList[column].partial_fit(X,yvar)
+			else:
+				self.hoeffdingTreesList.append(HoeffdingTreeClassifier())
+				self.hoeffdingTreesList[column].partial_fit(X,yvar)
 		return self
 	
 	def predict(self, X):
-		c= self.h1.predict(X)
-		return c
+		results = []
+		c = None
+		for i in range(len(self.hoeffdingTreesList)):
+			c = self.hoeffdingTreesList[i].predict(X)
+			results.append(c)
+		return results
 
 
 class HoeffdingTreeClassifier:
